23e3925a5ea952d08e0d1b1dac21f5968ced205a
Gio Palacino <gio@palacino.net>
2022-06-22 10:06:46 -0700

    feat(root-domain): adds root domain value access

--- lib/src/backend/config.dart
+++ lib/src/backend/config.dart
@@ -2,7 +2,11 @@ import 'dart:io';
 
 import '../common/config.dart';
 
+/// The base server configuration definition
 abstract class BaseServerConfig extends BaseConfig {
   BaseServerConfig([Map<String, String>? env])
       : super(env ?? Platform.environment);
+
+  /// Gets the `ROOT_DOMAIN` from the current context
+  String get domainRoot => getOrThrow('ROOT_DOMAIN');
 }
